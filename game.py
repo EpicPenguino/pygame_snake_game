3e8b15311dc6dcafd5691af92be56783fab0198b
EpicPenguino <epicpenguinogames@gmail.com>
2022-06-15 18:51:36 -0400

    added more logic

--- game.py
+++ game.py
@@ -1,28 +1,83 @@
 from pygame.locals import * 
 import pygame
+import time
+from random import randint
 
 class Player:
-    x = 500
-    y = 500
+    x = []
+    y = []
+    step = 1
+    direction = 0
+    length = 5
+    updates = 0
+    max_updates = 4
+    def __init__(self,length):
+        self.length = length
+        for i in range(length):
+            self.x.append(500),self.y.append(500)
+
+    def status(self):
+        self.updates += 1
+        if self.updates > self.max_updates:
+            # set previous x and y to the previous index in their respective arrays
+            for i in range(self.length-1,0,-1):
+                self.x[i],self.y[i] = self.x[i-1],self.y[i-1]
+        if self.direction == 0:
+            self.y -= self.step
+        if self.direction == 1:
+            self.y += self.step
+        if self.direction == 2:
+            self.x -= self.step
+        if self.direction == 3:
+            self.x += self.step
+
     def up(self):
-        self.y -= 1
+        self.direction=0
     def down(self):
-        self.y += 1
+        self.direction=1
     def left(self):
-        self.x -= 1
+        self.direction=2
     def right(self):
-        self.x += 1
+        self.direction=3
+    
+    def draw(self,background,image):
+        for i in range(self.length):
+            background.blit(image,(self.x[i],self.y[i]))
+
+class Apple:
+    x = 100
+    y = 100
+    step = 5
+
+    def __init__(self,x,y):
+        self.x = x*self.step
+        self.y = y*self.step
+
+    def draw(self,background,image):
+        background.blit(image,(self.x,self.y))
+
+class Game:
+    def collision(self,xA,yA,xS,yS,size):
+        if xA >= xS and xA <= xS+size:
+            if yA >= yS and yA <= yS+size:
+                return True
+        return False
 
 class SnakeGame:
     # window size is 1000x1000
     windowHeight = 1000
     windowWidth = 1000
-    player = Player()
+    player = 0
+    apple = 0
 
     def __init__(self):
         self._running = True
         self._display_surface = None
         self._image_surface = None
+        self._apple_surface = None
+        self.game = Game()
+        self.player = Player(5)
+        self.apple = Apple(100,100)
     
     def after_init(self):
         pygame.init()
@@ -30,11 +85,26 @@ class SnakeGame:
         self._display_surface = pygame.display.set_mode((self.windowWidth,self.windowHeight),pygame.HWSURFACE)
         pygame.display.set_caption("Python Snake Game")
         self._image_surface = pygame.image.load("snake.jpg").convert()
+        self._apple_surface = pygame.image.load("apple.png").convert()
     
     def render(self):
         self._display_surface.fill((50,50,100))
-        self._display_surface.blit(self._image_surface,(self.player.x,self.player.y))
+        self.player.draw(self._display_surface,self._image_surface)
+        self.apple.draw(self._display_surface,self._apple_surface)
         pygame.display.flip()
+
+    def loop(self):
+        self.player.status()
+        # collides with apple
+        for i in range(self.player.length):
+            if self.game.collision(self.apple.x,self.apple.y,self.player.x[i],self.player.y[i],5):
+                self.apple.x,self.apple.y = randint(30,180),randint(30,180)
+                self.player.length += 1
+        # collides with itself
+        for i in range(4,self.player.length):
+            if self.game.collision(self.player.x[0],self.player.y[0],self.player.x[i],self.player.y[i],1):
+                print("You lost.")
+                exit(0)
 
     def quit_event(self, event):
         if event == QUIT:
@@ -60,6 +130,7 @@ class SnakeGame:
             if keys[K_ESCAPE]:
                 self._running = False
             self.render()
+            time.sleep(100.0/1000.0)
         self.quit()
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-a = False
-
-while not a:
-    print("hi")
+# x = []
+# y = []
+# for i in range(3):
+#     x.append(500),y.append(500)
+# print(x,y)
+for i in range(4,-1,-1):
+    print(i)
